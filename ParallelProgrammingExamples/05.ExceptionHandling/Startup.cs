bd431053315199702c7cb55e0933755651eeee06
YakoYakov <yako.p.yakov@gmail.com>
2021-02-25 17:56:47 +0200

    Task exception handling example

---
+++ ParallelProgrammingExamples/05.ExceptionHandling/Startup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace _05.ExceptionHandling
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ExceptionHandling();
+            }
+            catch (AggregateException ae)
+            {
+                foreach (var ex in ae.InnerExceptions)
+                    Console.WriteLine($"Exception {ex.GetType()} handled elsewhere!");
+            }
+
+            Console.WriteLine("Main Program done.");
+            Console.ReadKey();
+        }
+
+        private static void ExceptionHandling()
+        {
+            Task t = Task.Factory.StartNew(() =>
+            {
+                throw new InvalidOperationException("Can`t do this") { Source = "t" };
+            });
+
+            Task t2 = Task.Factory.StartNew(() =>
+            {
+                throw new AccessViolationException("Can`t access this") { Source = "t2" };
+            });
+
+            // when you do Task.WaitAll() you get the exceptions
+
+            try
+            {
+                Task.WaitAll(t, t2);
+            }
+            catch (AggregateException ae)
+            {
+                //foreach (var ex in ae.InnerExceptions)
+                //    Console.WriteLine($"Exception {ex.GetType()} with message '{ex.Message}' from {ex.Source}");
+                ae.Handle(ae =>
+                {
+                    if (ae is InvalidOperationException)
+                    {
+                        Console.WriteLine("Invalid op handled");
+                        return true;
+                    }
+                    return false;
+                });
+            }
+        }
+    }
+}
