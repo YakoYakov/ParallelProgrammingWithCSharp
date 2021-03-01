b4345ed280e980d6e19b2e9b8d6d4aeda4212125
YakoYakov <yako.p.yakov@gmail.com>
2021-03-01 14:40:33 +0200

    Added Interlocked locked operation demo

---
+++ ParallelProgrammingExamples/07.InterlockOperations/Startup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _06.CriticalSections
+{
+    public class BankAccount
+    {
+        private int balance;
+
+        public int Balance 
+        {
+            get => balance;
+            private set => balance = value;
+        }
+
+        public void Deposit(int amount)
+        {
+            Interlocked.Add(ref balance, amount);
+        }
+
+        public void Withdraw(int amount)
+        {
+            Interlocked.Add(ref balance, -amount);
+        }
+    }
+
+
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            var tasks = new List<Task>();
+            var ba = new BankAccount();
+
+            for (int i = 0; i < 10; i++)
+            {
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    for (int j = 0; j < 1000; j++)
+                    {
+                        ba.Deposit(100);
+                    }
+                }));
+
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    for (int j = 0; j < 1000; j++)
+                    {
+                        ba.Withdraw(100);
+                    }
+                }));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            Console.WriteLine($"The current balance is {ba.Balance}");
+        }
+    }
+}
