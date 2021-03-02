e1d9a1a9c37b96d8d4bda5e30f8dd79bfb08fb4e
YakoYakov <yako.p.yakov@gmail.com>
2021-03-02 19:02:03 +0200

    Added Reader Writer Lock demo

---
+++ ParallelProgrammingExamples/10.ReaderWriterLocks/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _10.ReaderWriterLocks
+{
+    class Startup
+    {
+        static ReaderWriterLockSlim padLock = new ReaderWriterLockSlim();
+        static Random random = new Random();
+        static void Main(string[] args)
+        {
+            int x = 0;
+            List<Task> tasks = new List<Task>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                padLock.EnterReadLock();
+
+                Console.WriteLine($"Entered read lock, x = {x}");
+
+                Thread.Sleep(5000);
+
+                padLock.ExitReadLock();
+
+                Console.WriteLine($"Exited read lock, x = {x}");
+            }
+
+            try
+            {
+                Task.WaitAll(tasks.ToArray());
+            }
+            catch (AggregateException ae)
+            {
+                ae.Handle(e =>
+                {
+                    Console.WriteLine(e);
+                    return true;
+                });
+            }
+
+            while (true)
+            {
+                Console.ReadKey();
+                padLock.EnterWriteLock();
+                Console.WriteLine("Write lock acquired");
+                int newValue = random.Next(10);
+                x = newValue;
+                Console.WriteLine($"Set x = {x}");
+                padLock.ExitWriteLock();
+                Console.WriteLine("Write lock released");
+            }
+        }
+    }
+}
