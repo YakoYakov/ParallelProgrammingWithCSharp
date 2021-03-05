d704362c247a336e53037e245f3e634ce700ac29
YakoYakov <yako.p.yakov@gmail.com>
2021-03-05 16:45:43 +0200

    Added ConcurrentQueue example

---
+++ ParallelProgrammingExamples/12.ConcurrentQueue/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace _12.ConcurrentQueue
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            ConcurrentQueue<int> queue = new ConcurrentQueue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+
+            int result;
+            if (queue.TryDequeue(out result))
+                Console.WriteLine($"Successfully removed {result} from the queue");
+
+            if (queue.TryPeek(out result))
+                Console.WriteLine($"The front element of the queue is {result}");
+        }
+    }
+}
