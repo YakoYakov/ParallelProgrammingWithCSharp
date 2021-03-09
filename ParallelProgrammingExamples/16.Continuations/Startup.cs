d1f35595c3dba72548ca30a50b261bca51099e7e
YakoYakov <yako.p.yakov@gmail.com>
2021-03-09 18:36:03 +0200

    Added continuation demo

---
+++ ParallelProgrammingExamples/16.Continuations/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace _16.Continuations
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            Task boiling = Task.Factory.StartNew(() =>  Console.WriteLine("Boiling the water"));
+            Task usingBoilledWater = boiling.ContinueWith(t => {
+                Console.WriteLine($"Water booiling was finished task - {t.Id}");
+             });
+
+            usingBoilledWater.Wait();
+
+            Task<string> task = Task.Factory.StartNew(() => "Task 1");
+            Task<string> task2 = Task.Factory.StartNew(() => "Task 2");
+            
+            Task task3 = Task.Factory.ContinueWhenAll(new[] { task, task2 }, tasks =>
+            {
+                Console.WriteLine("Tasks completed:");
+                foreach (var result in tasks)
+                    Console.WriteLine(" - " + result.Result);
+                Console.WriteLine("All tasks done");
+            });
+
+            task3.Wait();
+        }
+    }
+}
