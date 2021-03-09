6d9cf1a12e49d4a6b027a47a203820ad7568079d
YakoYakov <yako.p.yakov@gmail.com>
2021-03-09 18:57:47 +0200

    Added child parent tasks demo

---
+++ ParallelProgrammingExamples/17.ChildTasks/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _17.ChildTasks
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            Task parent = new Task(() =>
+            {
+                Task child = new Task(() =>
+                {
+                    Console.WriteLine("Child task started");
+                    Thread.Sleep(3000);
+                    Console.WriteLine("Child task finished");
+                    //throw new Exception();
+                }, TaskCreationOptions.AttachedToParent);
+
+
+                var successHandler = child.ContinueWith(t =>
+                {
+                    Console.WriteLine("Child ran to completion");
+                }, TaskContinuationOptions.AttachedToParent | TaskContinuationOptions.OnlyOnRanToCompletion);
+
+                var errorHandler = child.ContinueWith(t =>
+                {
+                    Console.WriteLine("Child task failed");
+                }, TaskContinuationOptions.AttachedToParent | TaskContinuationOptions.OnlyOnFaulted);
+
+                child.Start();
+            });
+
+            parent.Start();
+
+            try
+            {
+                parent.Wait();
+            }
+            catch (AggregateException ae)
+            {
+                ae.Handle(e => true);
+            }
+        }
+    }
+}
